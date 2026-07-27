8c9bbab81e7327856dee39e84b6cf79b0bf9a685
pr0g3t <killianott4@gmail.com>
2026-07-27 06:56:39 +0200

    update

--- src/pages/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod home;
-pub mod legal_notice;
-pub mod privacy;
-pub mod terms;
-
-pub use home::Home;
-pub use legal_notice::LegalNotice;
-pub use privacy::PrivacyPolicy;
-pub use terms::TermsOfService;
